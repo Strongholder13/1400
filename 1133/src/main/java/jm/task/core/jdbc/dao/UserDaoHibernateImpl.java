6d660cfc7b01c55963e16ea079aa132abd5481d8
Strongholder13 <108480254+Strongholder13@users.noreply.github.com>
2022-09-08 08:15:54 +0300

    Update UserDaoHibernateImpl.java

--- 1133/src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ 1133/src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -6,65 +6,83 @@ import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import java.util.List;
 
+
+
+
 public class UserDaoHibernateImpl implements UserDao {
 
     private Session session = Util.getSessionFactory().openSession();
+
     public UserDaoHibernateImpl() {
+
     }
 
     @Override
     public void createUsersTable() {
         try {
-            session.beginTransaction();
             session.createSQLQuery("CREATE TABLE IF NOT EXISTS users (Id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(40), lastname VARCHAR(40), age INT)")
                     .executeUpdate();
-            session.getTransaction().commit();
-
+            
         } catch (HibernateException e) {
         }
+
     }
 
     @Override
     public void dropUsersTable() {
         try {
-            session.beginTransaction();
             session.createSQLQuery("DROP TABLE IF EXISTS users").executeUpdate();
-            session.getTransaction().commit();
+            
         } catch (HibernateException e) {
         }
     }
 
-   @Override
+    @Override
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try {
             session.beginTransaction();
             session.save(new User(name, lastName, age));
             session.getTransaction().commit();
+        } catch (HibernateException e) {
+            session.getTransaction().rollback();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        try {
             session.beginTransaction();
             User user = session.get(User.class, id);
             session.delete(user);
             session.getTransaction().commit();
+        } catch (HibernateException e) {
+            session.getTransaction().rollback();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        List<User> users ;
-        session.beginTransaction();
-        users = session.createQuery("select i from User i").list();
-        session.getTransaction().commit();
-        return users;
+        try {
+            List<User> users;
+            session.beginTransaction();
+            users = session.createQuery("select i from User i").list();
+            session.getTransaction().commit();
+            return users;
+        } catch (HibernateException e) {
+            session.getTransaction().rollback();
+            return null;
+        }
     }
 
     @Override
     public void cleanUsersTable() {
-
-        session.beginTransaction();
-        session.createSQLQuery("TRUNCATE TABLE users").executeUpdate();
-        session.getTransaction().commit();
+        try {
+            session.beginTransaction();
+            session.createSQLQuery("TRUNCATE TABLE users").executeUpdate();
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            session.getTransaction().rollback();
+        }
     }
 }

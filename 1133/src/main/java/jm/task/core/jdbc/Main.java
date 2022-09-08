dd501c91d2268dc9083f656fd3a707fea47fc14a
Strongholder13 <108480254+Strongholder13@users.noreply.github.com>
2022-09-08 22:22:17 +0300

    Update Main.java

--- 1133/src/main/java/jm/task/core/jdbc/Main.java
+++ 1133/src/main/java/jm/task/core/jdbc/Main.java
@@ -7,32 +7,20 @@ import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
-//        UserService userService = new UserServiceImpl();
-//        userService.createUsersTable();
-//        userService.saveUser("Name1", "LastName1", (byte) 20);
-//        userService.saveUser("Name2", "LastName2", (byte) 25);
-//        userService.saveUser("Name3", "LastName3", (byte) 31);
-//        userService.saveUser("Name4", "LastName4", (byte) 38);
-//        userService.removeUserById(2);
-//        userService.getAllUsers();
-////        userService.dropUsersTable();
-//        userService.cleanUsersTable();
-//        userService.dropUsersTable();
+
 
 
         UserService userService2 = new UserServiceImpl();
-        //userService2.dropUsersTable();
+        userService2.dropUsersTable();
         userService2.createUsersTable();
         userService2.saveUser("Name101", "LastName01", (byte) 20);
         userService2.saveUser("Name22", "LastName02", (byte) 25);
         userService2.saveUser("Name33", "LastName03", (byte) 31);
         userService2.saveUser("Name44", "LastName04",  (byte)38);
-    //      userService2.removeUserById(4);
+        userService2.removeUserById(4);
         userService2.getAllUsers();
-//
-
-//       userService2.cleanUsersTable();
-//    userService2.dropUsersTable();
+        userService2.cleanUsersTable();
+        userService2.dropUsersTable();
 
     }
 }
